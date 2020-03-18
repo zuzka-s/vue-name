d0369d41bd3e30c38d3fc2ef0b522581792263ae
Zuzana Sommerová <zuzana.sommerova@mall.cz>
2020-03-18 19:08:12 +0100

    reseni

---
+++ NameEditor.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p>
+      first name:
+      <input v-model="editableName" />
+    </p>
+    <p>
+    </p>
+     <p>
+      last name:
+      <input v-model="editableLastName" />
+    </p>
+    <p>
+      {{ editableName }} {{ editableLastName }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NameEditor',
+  data () {
+    return {
+      editableName: this.firstName,
+      editableLastName: this.lastName
+    }
+  },
+  props: {
+    firstName: {
+      type: String,
+      default: 'John'
+    },
+    lastName: {
+      type: String,
+      default: 'Doe'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NameEditor />
+  </div>
+</template>
+
+<script>
+import NameEditor from './NameEditor'
+
+export default {
+  components: { NameEditor },
+
+}
+</script>
+
+<style>
+
+</style>
